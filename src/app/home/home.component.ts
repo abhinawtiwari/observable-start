5718b6e1c95876a5407d64ddf0205693dbc6d11a
Abhinaw Tiwari <abhinawandyou@gmail.com>
2019-01-08 20:55:26 +0530

    rxjs-compat removal and code changes to comply with Angular 6 way of rxjs implementation.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
-import { Observer } from 'rxjs/Observer';
-import { Subscription } from 'rxjs/Subscription';
+import { Observable, Observer, Subscription, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -16,12 +14,12 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000)
-      .map(
+    const myNumbers = interval(1000)
+      .pipe(map(
         (data: number) => {
           return data * 2;
         }
-      );
+      ));
     this.numbersObsSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
@@ -48,7 +46,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    //this.numbersObsSubscription.unsubscribe();
+    this.numbersObsSubscription.unsubscribe();
     this.customObsSubscription.unsubscribe();
   }
 
